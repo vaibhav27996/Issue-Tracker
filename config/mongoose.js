4d87ef23b436d62bf9c1aad40ba666342c4c148d
vaibhav27996 <vmahajan27996@gmail.com>
2023-03-11 16:12:21 +0530

    some changed in mongoose

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
-const env=require('./environment');
-mongoose.connect(`mongodb://localhost/${env.db}`);
+// const env=require('./environment');
+// mongoose.connect(`mongodb://localhost/${env.db}`);
 
 // mongoose.connect(`${process.env.MONGO_URl}`, {useNewUrlParser: true, useUnifiedTopology: true}, ()=>{
 //     console.log("mongodb is connected");
 // });
-
+mongoose.connect(process.env.MONGO_URl);
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
